95d8f5974b0bdf08a01af5acfe3748a29c777a54
ht8hcrmswh-hue <ht8hcrmswh@privaterelay.appleid.com>
2026-07-13 13:14:48 +0900

    Deploy refined field and map layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3650105624" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "955154828" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
