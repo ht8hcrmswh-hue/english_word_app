00a5e43780527edd74ad7b9e6998f8913b4e638e
ht8hcrmswh-hue <ht8hcrmswh@privaterelay.appleid.com>
2026-07-19 13:17:41 +0900

    Deploy normal incorrect answer retry

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1738433683" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1689575696" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
