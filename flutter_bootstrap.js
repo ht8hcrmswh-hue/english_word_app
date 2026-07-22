a441546e56a68ca0cabd9e6bbc3fa23f762313d7
ht8hcrmswh-hue <ht8hcrmswh@privaterelay.appleid.com>
2026-07-22 12:14:32 +0900

    Deploy production vocabulary fields and classic gold donut

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1800930316" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1693879707" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
