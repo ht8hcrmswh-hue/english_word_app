95285beb8ef21b6c32b7822acfa8f3abd926273e
ht8hcrmswh-hue <ht8hcrmswh@privaterelay.appleid.com>
2026-08-01 17:28:44 +0900

    Deploy iOS keyboard level name updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "105572305" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "996860839" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
