2a3b5d6b758635f5aa5c46fb31dc9463f3b5181c
ht8hcrmswh-hue <ht8hcrmswh@privaterelay.appleid.com>
2026-08-10 04:08:39 +0900

    Deploy refined level actions controls

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3099402557" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4089725053" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
