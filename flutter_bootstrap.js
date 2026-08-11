224feac953485ca49595c8300916221d7c09da14
ht8hcrmswh-hue <ht8hcrmswh@privaterelay.appleid.com>
2026-08-11 13:15:17 +0900

    Deploy reviewed Japanese answer updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2442784279" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "178428331" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
